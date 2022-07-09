3f456d4d1b526585fa91859c2442023a07e6b220
tungjj <tungplatin@gmail.com>
2022-07-09 11:08:11 +0700

    fix: cloundinary

---
+++ src/cloudinary/cloudinary.constants.ts
@@ -0,0 +1,8 @@
+export const CLOUDINARY = 'Cloudinary';
+
+export const config = {
+  cloud_name: 'dpdag8dpx',
+  api_key: '896215283124184',
+  api_secret: '6pSnkXLyUe6lB1zuEDMNkbujaQA',
+  secure: true,
+};
